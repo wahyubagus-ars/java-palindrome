372823b2d41eb6d353aa3ef0c02afaa1b1740067
wahyubagus <wahyu.sulaksono@ist.id>
2023-05-05 08:17:47 +0700

    feature: find the longest palindromic substring in a given string

---
+++ Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static String longestPalindromeSubstring(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        String res = "";
+        int n = s.length();
+
+        /** change all input to lowercase to handle Lowercase and Uppercase. Point no. 3 **/
+        s = s.toLowerCase();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                String sub = s.substring(i, j + 1);
+                boolean isPalindrome = true;
+
+                int k = 0, v = sub.length() - 1;
+                while (k < v) {
+                    if (sub.charAt(k) != sub.charAt(v)) {
+                        isPalindrome = false;
+                    }
+                    k++;
+                    v--;
+                }
+                if (isPalindrome && (res.length() == 0 || sub.length() > res.length())) {
+                    res = sub;
+                }
+            }
+        }
+        return res.toLowerCase();
+    }
+
+    private static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "babad";
+        String result = longestPalindromeSubstring(input);
+        System.out.println(result);
+    }
+}
